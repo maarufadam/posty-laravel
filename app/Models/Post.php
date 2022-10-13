96d304149ae26b0e35fa36205d5c646e39abf4e6
Ma'aruf Mohammed Adamu <63424383+maarufadam@users.noreply.github.com>
2022-10-13 21:24:14 +0100

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'body',
+        'user_id',
+    ];
+
+    protected $cast = [
+        'user_id' => 'integer',
+    ];
+
+    public function likedBy(User $user)
+    {
+        return $this->likes->contains('user_id', $user->id);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+}
